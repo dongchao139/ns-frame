1b2692d0fc8658a3250dc948029f33c5aaf5a71b
donnc <donnchao@outlook.com>
2019-06-16 23:22:40 +0800

    add Clarity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MenuService } from './home/MenuService';
 import { DynamicLoadDirective } from './home/DynamicLoadDirective';
 import { NgModule } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import {BrowserModule} from '@angular/platform-browser';
     BrowserModule,
     AppRoutingModule,
     NsForm,
-    NsMenuModule
+    NsMenuModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [MenuService],
   bootstrap: [AppComponent]
